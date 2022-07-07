27d699b4e7861f77ea7af590537ee3bba5dcd210
dhineshprabakaran <101916123+dhineshprabakaran@users.noreply.github.com>
2022-07-07 21:46:52 +0530

    Create ifelse.go

---
+++ ifelse.go
@@ -0,0 +1,17 @@
+//07072022
+//if-else condition
+
+package main
+
+import "fmt"
+
+func main() {
+	var role = "Solutions Architect"
+	if role != "Sol Arch" {
+		fmt.Println("undefined role")
+	} else {
+
+		fmt.Println(role)
+	}
+
+}
